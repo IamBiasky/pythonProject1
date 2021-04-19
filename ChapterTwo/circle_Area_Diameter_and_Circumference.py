9c0cc3b64ddc8c53f168e38756bc4fbcc6adf71a
Isaac John <ijenubiak@gmail.com>
2021-04-20 00:57:30 +0100

    Chapter 2 Exercises

--- ChapterTwo/circle_Area_Diameter_and_Circumference.py
+++ ChapterTwo/circle_Area_Diameter_and_Circumference.py
@@ -4,7 +4,7 @@ circumference = 2 * 3.14159 * radius
 area = 3.14159 * radius ** 2
 
 
-print("For a Circle of radius 2")
+print("Given a Circle of radius 2")
 print("The Area is " + str(area))
 print("The Diameter is " + str(diameter))
 print("The Circumference is " + str(circumference))
