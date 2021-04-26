807089388bd8616821a5d35b19019921319e08af
Isaac John <ijenubiak@gmail.com>
2021-04-27 00:03:49 +0100

    Chapter Two Exercises

---
+++ ChapterTwo/Tables_Of_Squares_Cube.py
@@ -0,0 +1,4 @@
+print("Number   Squares    Cube")
+
+for number in range(0, 6):
+    print(number,   "      ", number ** 2,     "        ", number ** 3)
---
+++ ChapterTwo/odd_or_Even.py
@@ -0,0 +1,12 @@
+user_int = 0
+
+while user_int != 1:
+    user_int = int(input('Enter Your Number Here:'))
+
+    if user_int % 2 == 0:
+        print("Number is Even")
+
+    elif user_int % 2 == 1:
+        print("Number is Odd")
+
+# user_int += 1
