dbc2817ff95b4356b5029529d6799caec2f00862
Isaac John <ijenubiak@gmail.com>
2021-06-16 16:10:32 +0100

    Deitel Exercises

---
+++ ChapterTwo/SeparatingDigits.py
@@ -0,0 +1,20 @@
+number = int(input("Enter a number: "))
+second = int(input("Enter a number: "))
+third = int(input("Enter a number: "))
+fourth = int(input("Enter a number: "))
+fifth = int(input("Enter a number: "))
+
+if 9999 < number < 100000:
+    fifth = number % 10
+    number //= 10
+    fourth = number % 10
+    number //= 10
+    third = number % 10
+    number //= 10
+    second = number % 10
+    number //= 10
+# else:
+#     print("Invalid input")
+
+
+print(number, "   ", second, "   ", third, "   ", fourth, "   ", fifth)
