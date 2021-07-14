944270c272eee784ce8803fa373089655330bf38
Isaac John <ijenubiak@gmail.com>
2021-07-14 15:05:56 +0100

    Self Practice

--- SelfPractice/function_exponent.py
+++ SelfPractice/function_exponent.py
@@ -1,3 +1,6 @@
+# Write a function called exponent(base, exp)
+# that returns an int value of base raises to the power of exp
+
 def exponent(base, exp):
     exp_int = base ** exp
     return exp_int
--- SelfPractice/multiplication_table.py
+++ SelfPractice/multiplication_table.py
@@ -1,3 +1,5 @@
+# Print multiplication table from 1 to 10.
+
 print(" ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
 
 for number in range(1, 12):
