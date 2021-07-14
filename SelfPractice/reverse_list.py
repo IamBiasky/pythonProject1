4d2f4097fdba1070e55c55dbb6606af9f8e12a98
Isaac John <ijenubiak@gmail.com>
2021-07-14 15:01:21 +0100

    Self Practice

---
+++ SelfPractice/reverse_list.py
@@ -0,0 +1,11 @@
+# Given a list, reverse it.
+
+
+list_of_numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+
+print(list_of_numbers)
+
+order = []
+
+for i in list_of_numbers[::-1]:
+    print(i, end=" ")
