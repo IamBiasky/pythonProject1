53cefe33815e09085dd00624e09eb54d7806cad8
Isaac John <ijenubiak@gmail.com>
2021-07-14 14:53:04 +0100

    Self Practice

--- SelfPractice/function_exponent.py
+++ SelfPractice/function_exponent.py
@@ -1,6 +1,9 @@
 def exponent(base, exp):
-    return base ** exp
+    exp_int = base ** exp
+    return exp_int
 
 
-base_str = int(input("Please enter an integer: "))
-exponent_str = int(input("Please enter an integer: "))
+base = int(input("Please enter a base integer: "))
+exp = int(input("Please enter an exponent integer: "))
+
+print(exponent(base, exp))
