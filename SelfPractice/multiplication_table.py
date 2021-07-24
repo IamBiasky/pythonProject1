0ed3a61a7b784a65fc8a2ad7da78527c5b6b2021
Isaac John <ijenubiak@gmail.com>
2021-07-24 01:54:59 +0100

    Self Practice

--- SelfPractice/multiplication_table.py
+++ SelfPractice/multiplication_table.py
@@ -1,7 +1,8 @@
-# Print multiplication table from 1 to 10.
+# print('multiplication table from 1 to 10.')
 
-print(" ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
+print(f'{" ":>1}  {"1":>4} {"2":>4} {"3":>4} {"4":>4} {"5":>4} {"6":>4} {"7":>4} {"8":>4} {"9":>4} {"10":>4} {"11":>4}\
+ {"12":>4}')
 
 for number in range(1, 12):
-    print(number, number*1, number*2, number*3, number*4, number*5, number*6, number*7, number*8, number*9, number*10,
-          number*11, number*12)
+    print(f'{number:>2} {number*1:>4} {number*2:>4} {number*3:>4} {number*4:>4} {number*5:>4} {number*6:>4}\
+ {number*7:>4} {number*8:>4} {number*9:>4} {number*10:>4} {number*11:>4} {number*12:>4}')
